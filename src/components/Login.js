2ac36f59ba0093e73b390f812167e4d9d5f0185e
Shamima Shammi <t2shsh00@students.oamk.fi>
2023-12-07 03:50:25 +0200

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.css';
 
-axios.defaults.baseURL = `${process.env.BACKEND_API_URL}`;
+axios.defaults.baseURL = `https://gogoship.azurewebsites.net`;
 
 const Login = () => {
     // State variables to hold email, password, and error message
